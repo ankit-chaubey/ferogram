98c05769b2cdd9b076e316e71d3b5340c513f3e2
ankit-chaubey <ankit-chaubey@users.noreply.github.com>
2026-06-24 16:28:57 +0000

    deploy: a0f83a1db8fb36081459ca76eb05c708544045ad

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d270a347.js',
+                path_to_root + 'searchindex-6c28794d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
